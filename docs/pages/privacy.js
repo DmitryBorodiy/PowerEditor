daba4b7be9d3b99bc296e93f6b13f61809ec20a1
Dmitriy <57910623+DmitryBorodiy@users.noreply.github.com>
2022-10-01 23:38:18 +0300

    privacy policy update

---
+++ docs/pages/privacy.js
@@ -0,0 +1,44 @@
+﻿import {
+  StandardLuminance,
+  baseLayerLuminance,
+  fillColor,
+  allComponents,
+  provideFluentDesignSystem
+} from "https://unpkg.com/@fluentui/web-components@2.0.0";
+
+provideFluentDesignSystem().register(allComponents);
+
+//UI elements
+var rootLayout = document.getElementById("rootLayout");
+
+window.loaded = function(){
+  Page_OnLoaded();
+};
+
+function ThemeInitialize(){
+  try{
+    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+
+    if(darkThemeMq.matches) {
+      baseLayerLuminance.setValueFor(rootLayout, StandardLuminance.DarkMode);
+      return "dark";
+    } else {
+      baseLayerLuminance.setValueFor(rootLayout, StandardLuminance.LightMode);
+      return "light";
+    }
+  }
+  catch(e){
+    console.log(e.toString());
+  }
+}
+
+function Page_OnLoaded(){
+  try{
+    ThemeInitialize();
+  }
+  catch(e){
+    console.log(e.toString());
+  }
+}
+
+Page_OnLoaded();
